ec2d29904bbb350040b791e6f663093bfe318bd6
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-08-26 08:33:50 +0530

    Create TA2 Sol.py

---
+++ TA2 Sol.py
@@ -0,0 +1,15 @@
+class Dog:
+  owner=True
+  teeth=42
+  happy_wagging_tail=True
+  
+tommy=Dog()
+print(tommy.owner)
+print(tommy.happy_wagging_tail)
+print(tommy.teeth)
+
+tommy=Dog()
+scooby=Dog()
+maxi=Dog()
+print(tommy.owner)
+print(scooby.teeth)
